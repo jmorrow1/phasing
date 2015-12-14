6f06d87fc46ff86f9e996e60b7bb5801e5ae2637
James Morrow <jmorrow1@unca.edu>
2015-12-13 17:06:42 -0700

    Support for manipulating a KeyboardsView setting (superimposeKeyboards).

--- src/views/KeyboardsView.java
+++ src/views/KeyboardsView.java
@@ -10,35 +10,27 @@ public class KeyboardsView extends View {
 	//music
 	private PhraseReader readerA, readerB;
 	//piano
+	private boolean superimposeKeyboards;
 	private Piano keyboardA, keyboardB;
 	private Piano keyboardAB;
 	private final int PIANO_SIZE = 50;
 	//piano players
 	private PianoPlayer pianoPlayerA, pianoPlayerB;
 	//conversion
-	private int firstPitchOfPiano = 60;
+	private int firstPitchOfPiano;
+	
+	protected void init() {
+		firstPitchOfPiano = 60;
+		initPianos();
+		initPianoPlayers();
+		initPhraseReaders();
+	}
 	
 	public KeyboardsView(Rect rect, Phrase phrase, int color1, int color2, int opacity, PApplet pa) {
 		super(rect, phrase, color1, color2, opacity, 0, pa);
-		
-		this.phrase = phrase;
-		
-		initPianos(true);
-		initPianoPlayers(true);
-		
-		//init phrase readers
-		try {
-			readerA = new PhraseReader(phrase, -1, pianoPlayerA,
-					PianoPlayer.class.getMethod("setActiveKey", PhraseReader.class));
-			readerB = new PhraseReader(phrase, -1, pianoPlayerB,
-					PianoPlayer.class.getMethod("setActiveKey", PhraseReader.class));
-
-		} catch (NoSuchMethodException | SecurityException e) {
-			e.printStackTrace();
-		}
 	}
 	
-	private void initPianos(boolean superimposeKeyboards) {
+	private void initPianos() {
 		if (superimposeKeyboards) {
 			keyboardAB = new Piano(2, new Rect(this.getCenx(), this.getCeny(),
 					0.75f*this.getWidth(), PIANO_SIZE, PApplet.CENTER), true, pa.color(255));
@@ -52,7 +44,7 @@ public class KeyboardsView extends View {
 		}
 	}
 	
-	private void initPianoPlayers(boolean superimposeKeyboards) {
+	private void initPianoPlayers() {
 		if (superimposeKeyboards) {
 			pianoPlayerA = new PianoPlayer(color1, keyboardAB, phrase);
 			pianoPlayerB = new PianoPlayer(color2, keyboardAB, phrase);
@@ -62,13 +54,26 @@ public class KeyboardsView extends View {
 			pianoPlayerB = new PianoPlayer(color2, keyboardB, phrase);
 		}
 	}
+	
+	private void initPhraseReaders() {
+		//init phrase readers
+		try {
+			readerA = new PhraseReader(phrase, -1, pianoPlayerA,
+					PianoPlayer.class.getMethod("setActiveKey", PhraseReader.class));
+			readerB = new PhraseReader(phrase, -1, pianoPlayerB,
+					PianoPlayer.class.getMethod("setActiveKey", PhraseReader.class));
+
+		} catch (NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Override
 	public void update(float dNotept1, float dNotept2, int sign) {
 		readerA.update(dNotept1);
 		readerB.update(dNotept2);
 		
-		if (keyboardAB != null) {
+		if (superimposeKeyboards) {
 			keyboardAB.drawWhiteKeys(pa);
 			pianoPlayerA.displayIfWhite(pa);
 			pianoPlayerB.displayIfWhite(pa);
@@ -76,13 +81,12 @@ public class KeyboardsView extends View {
 			pianoPlayerA.displayIfBlack(pa);
 			pianoPlayerB.displayIfBlack(pa);
 		}
-		if (keyboardA != null) {
+		else {
 			keyboardA.drawWhiteKeys(pa);
 			pianoPlayerA.displayIfWhite(pa);
 			keyboardA.drawBlackKeys(pa);
 			pianoPlayerA.displayIfBlack(pa);
-		}
-		if (keyboardB != null) {
+
 			keyboardB.drawWhiteKeys(pa);
 			pianoPlayerB.displayIfWhite(pa);
 			keyboardB.drawBlackKeys(pa);
@@ -130,16 +134,25 @@ public class KeyboardsView extends View {
 	
 	/*Settings*/
 	
-	/*private void superimposeKeyboards(boolean superimposeKeyboards) {
-		initPianos(superimposeKeyboards);
-		initPianoPlayers(superimposeKeyboards);
-	}*/
+	private void superimposeKeyboards(boolean superimposeKeyboards) {
+		this.superimposeKeyboards = superimposeKeyboards;
+		initPianos();
+		initPianoPlayers();
+		initPhraseReaders();
+	}
 	
 	public void loadPreset(int preset) {
-		
+		switch (preset) {
+			case 0 :
+				superimposeKeyboards(true);
+				break;
+			case 1 :
+				superimposeKeyboards(false);
+				break;
+		}
 	}
 	
 	public int numPresets() {
-		return 1;
+		return 2;
 	}
 }
